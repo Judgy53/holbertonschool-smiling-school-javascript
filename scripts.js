dd47cd6e05eae56db9942f9a7835df66a69e9b91
Judgy53 <alexandre.gaubert94@orange.fr>
2024-05-30 17:39:00 +0200

    Task 5: Courses

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let searchTimeout = null;
+const searchData = {};
+
 function buildQuoteHTML(quote) {
     return `<div class="row mx-auto align-items-center">
         <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
@@ -39,6 +42,30 @@ function buildVideoHTML(video) {
     </div>`;
 }
 
+function carouselSlick(carouselSelector) {
+    $(carouselSelector).slick({
+        autoplay: true,
+        arrows: true,
+        prevArrow: '<a class="carousel-control-prev arrow-left" role="button"><img src="images/arrow_black_left.png" alt="Previous" aria-hidden="true"/><span class="sr-only">Previous</span></a>',
+        nextArrow: '<a class="carousel-control-next arrow-right" role="button"><img src="images/arrow_black_right.png" alt="Next" aria-hidden="true"/><span class="sr-only">Next</span></a>',
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+}
+
 function fetchCarouselData(carouselSelector, fetchURL, entryTemplate, slick = false) {
     const $carousel = $(carouselSelector);
     if (!$carousel.length)
@@ -68,27 +95,76 @@ function fetchCarouselData(carouselSelector, fetchURL, entryTemplate, slick = fa
     });
 }
 
-function carouselSlick(carouselSelector) {
-    $(carouselSelector).slick({
-        autoplay: true,
-        arrows: true,
-        prevArrow: '<a class="carousel-control-prev arrow-left" role="button"><img src="images/arrow_black_left.png" alt="Previous" aria-hidden="true"/><span class="sr-only">Previous</span></a>',
-        nextArrow: '<a class="carousel-control-next arrow-right" role="button"><img src="images/arrow_black_right.png" alt="Next" aria-hidden="true"/><span class="sr-only">Next</span></a>',
-        slidesToShow: 4,
-        responsive: [
-            {
-                breakpoint: 1000,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
+function fetchSearchResult(initialSearch = false) {
+    const $resultContainer = $('section.results .row');
+    const $videoCount = $('section.results .section-title span');
+
+    $resultContainer.empty();
+    $resultContainer.after($('<div class="loader">'));
+    $videoCount.hide();
+
+    $.get('https://smileschool-api.hbtn.info/courses', searchData, function(results) {
+        if (initialSearch) {
+            fillDropdown('#menu-link-search-topic span', results.topic, '#input-search-topic', results.topics);
+            fillDropdown('#menu-link-search-sort span', results.sort, '#input-search-sort', results.sorts);
+        }
+        $('#input-search-keywords').val(results.q);
+
+        for (const course of results.courses) {
+            const $courseElem = $('<div class="col-12 col-sm-4 col-lg-3 d-flex justify-content-center">').append(buildVideoHTML(course));
+            $resultContainer.append($courseElem);
+        }
+
+        $resultContainer.siblings('.loader').remove();
+        $videoCount.text(`${results.courses.length} video${results.courses.length > 1 ? 's': ''}`);
+        $videoCount.show();
+    });
+}
+
+function fillDropdown(currentValueSelector, currentValue, optionListSelector, optionList) {
+    $(currentValueSelector).text(fancyDropdownValue(currentValue));
+
+    for (const option of optionList) {
+        $(optionListSelector).append($(`<a class="dropdown-item" href="#" data-value=${option}>${fancyDropdownValue(option)}</a>`));
+    }
+}
+
+function fancyDropdownValue(value) {
+    const words = value.split('_');
+    
+    for (const index in words) {
+        const orig = words[index];
+        if (orig.length == 0)
+            continue;
+
+        const firstLetter = orig[0].toUpperCase();
+        words[index] = firstLetter + orig.substring(1);
+    }
+
+    return words.join(' ');
+}
+
+function delayedSearch() {
+    if (searchTimeout)
+        clearTimeout(searchTimeout);
+
+    searchData.q = $('#input-search-keywords').val();
+    searchTimeout = setTimeout(fetchSearchResult, 500);
+}
+
+function addSearchEvents() {
+    $('#input-search-keywords').on('input', delayedSearch);
+    $(document).on('click', '#input-search-topic a', function() {
+        const value = $(this).text();
+        searchData.topic = $(this).data('value');
+        $('#menu-link-search-topic span').text(value);
+        fetchSearchResult();
+    });
+    $(document).on('click', '#input-search-sort a', function() {
+        const value = $(this).text();
+        searchData.sort = $(this).data('value');
+        $('#menu-link-search-sort span').text(value);
+        fetchSearchResult();
     });
 }
 
@@ -96,4 +172,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchCarouselData('#carousel-quotes', 'https://smileschool-api.hbtn.info/quotes', buildQuoteHTML);
     fetchCarouselData('#carousel-tutorials', 'https://smileschool-api.hbtn.info/popular-tutorials', buildVideoHTML, true);
     fetchCarouselData('#carousel-videos', 'https://smileschool-api.hbtn.info/latest-videos', buildVideoHTML, true);
+    fetchSearchResult(true);
+    addSearchEvents();
 });
